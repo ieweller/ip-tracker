a804fa1a92fa1238bf71502d8dd827a5299935dc
Ivan Weller <ivan@wellerindependent.com>
2020-11-06 05:10:43 -0800

    added hover states, refined and finalized styling to spec, removed form validation and added search by domain, cleaned up code. project now to spec

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,16 @@
-var api_key = "at_C4SDN7bt74w5infGoMPMzdtIIJd2w";
-
 var lat = "";
 var lng = "";
 
+$(function() { update(); });
 
-function update(ip) {
+function update(input) {
 
   $.ajax({
+
     url: "https://geo.ipify.org/api/v1",
-    data: {apiKey: api_key, ipAddress: ip},
+    data: {apiKey: "at_C4SDN7bt74w5infGoMPMzdtIIJd2w", ipAddress: input, domain: input},
     success: function(data) {
+
       document.getElementById("ip-address").innerHTML = data.ip;
       document.getElementById("location").innerHTML = data.location.city + ", " + data.location.region + " " + data.location.postalCode;
       document.getElementById("timezone").innerHTML = data.location.timezone + " UTC";
@@ -18,34 +19,38 @@ function update(ip) {
       lat = data.location.lat;
       lng = data.location.lng;
 
-      var viewlat = lat + 0.005;
+      var viewlat = lat + 0.008;
 
       mapboxgl.accessToken = 'pk.eyJ1Ijoid2VsbGVyaW5kZXAiLCJhIjoiY2toNHhsbmgzMGczdDMwcWJqODVyd2FyeSJ9.io83u9yMFfuKLtBMrsaGNA';
+
       var map = new mapboxgl.Map({
+
         container: 'mapid',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [lng, viewlat],
         zoom: 14,
         interactive: false
+
       });
 
-      var marker = new mapboxgl.Marker({
-        color: '#000000'
-      })
+      var marker = new mapboxgl.Marker({ color: '#000000' })
+
       .setLngLat([lng, lat])
       .addTo(map);
+
     }
   });
 
 }
 
 document.getElementById('ip-form').onsubmit = function() {
-  var input = document.getElementById('search').value;
-  document.getElementById('search').value = "";
-  update(input);
-  return false;
-};
 
-$(function() {
-  update();
-});
+  var input = document.getElementById('search').value;
+
+  document.getElementById('search').value = "";
+
+  update(input);
+
+  return false;
+
+};
